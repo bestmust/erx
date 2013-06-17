fce6b0eaf5c8394213284d015acbf2327c24e804
Mustaqim Moulavi <bestmust@gmail.com>
2013-06-17 01:25:44 -0700

    added site php code

---
+++ site/www/public_html/android_api/include/DB_Connect.php
@@ -0,0 +1,34 @@
+<?php
+ 
+class DB_Connect {
+ 
+    // constructor
+    function __construct() {
+         
+    }
+ 
+    // destructor
+    function __destruct() {
+        // $this->close();
+    }
+ 
+    // Connecting to database
+    public function connect() {
+        require_once 'config.php';
+		
+        $con = mysql_connect($mysql_host, $mysql_user, $mysql_password);
+        // selecting database
+        mysql_select_db($mysql_database);
+ 
+        // return database handler
+        return $con;
+    }
+ 
+    // Closing database connection
+    public function close() {
+        mysql_close();
+    }
+ 
+}
+ 
+?>
---
+++ site/www/public_html/android_api/include/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Database config variables
+*/
+$mysql_host = "mysql16.000webhost.com";
+$mysql_database = "a8748905_android";
+$mysql_user = "a8748905_root";
+$mysql_password = "tiger123";
+ 
+ 
+ /*
+ 
+define("DB_HOST", "10.0.0.9");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "android_api");
+*/
+?>
